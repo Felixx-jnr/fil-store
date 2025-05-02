b31cf660b4687e214f4bc1b23374f4c2fd28305e
Felixx-jnr <uyuoukoh@gmail.com>
2025-05-02 02:49:45 +0100

    Implemented import fix

--- app/api/auth/verify/route.js
+++ app/api/auth/verify/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/db";
-import PendingVerification from "@/models/PendingVerification";
+import PendingVerification from "../../../../models/PendingVerification";
 import User from "@/models/User";
 import { sendEmail } from "@/lib/mailer";
 
