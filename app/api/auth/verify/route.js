4bed471e2530ae1130992068cd7a346b6e22d78e
Felixx-jnr <uyuoukoh@gmail.com>
2025-05-09 23:25:25 +0100

    Implemented rename

--- app/api/auth/verify/route.js
+++ app/api/auth/verify/route.js
@@ -1,7 +1,7 @@
 import { connectDB } from "@/lib/db";
 import User from "@/models/User";
 import { sendEmail } from "@/lib/mailer";
-import PendingVerification from "@/models/PendingVerification.js";
+import PendingVerification from "@/models/PendingVerification";
 
 export async function POST(req) {
   await connectDB();
