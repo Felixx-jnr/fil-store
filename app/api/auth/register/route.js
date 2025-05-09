46b51f3c4b87fa0b587e32d8b026f66312b7cc1a
Felixx-jnr <uyuoukoh@gmail.com>
2025-05-09 23:05:52 +0100

    Implemented js on models

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -2,7 +2,7 @@ import { connectDB } from "@/lib/db";
 import bcrypt from "bcryptjs";
 import { generateCode } from "@/lib/utils";
 import { sendEmail } from "@/lib/mailer";
-import PendingVerification from "@/models/PendingVerification";
+import PendingVerification from "@/models/PendingVerification.js";
 
 export async function POST(req) {
   await connectDB();
--- app/api/auth/verify/route.js
+++ app/api/auth/verify/route.js
@@ -1,7 +1,7 @@
 import { connectDB } from "@/lib/db";
 import User from "@/models/User";
 import { sendEmail } from "@/lib/mailer";
-import PendingVerification from "@/models/PendingVerification";
+import PendingVerification from "@/models/PendingVerification.js";
 
 export async function POST(req) {
   await connectDB();
